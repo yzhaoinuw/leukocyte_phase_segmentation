added accuracy calculation , confusion matrix in logging; fixed validation error logging; made changes to early stopping criteria; added ensembling and code to visualize predictions.

predict.py
@@ -5,61 +5,94 @@ Created on Tue May  9 11:03:50 2023
 @author: Yue
 """
 
+import logging
+
 from tqdm import tqdm
+
 import torch
-from torch import nn, autocast
+from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from torchmetrics import ConfusionMatrix, Dice
-from sklearn.model_selection import KFold
+from torchmetrics import ConfusionMatrix
 
-from utils import show_image
+from utils import show_image, calculate_dice, calculate_accuracy
 from unet import UNet
-from dataset import SegmentationDataset, Subset
+from dataset import SegmentationDataset
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DATA_PATH = "./data/test"
-SPLIT_NUM = 0
-MODEL_PATH = f"./model/split_{SPLIT_NUM}/"
-model_name = "model_CELoss_weighted_29.pth"
+SPLIT_NUM = 4
+CLASS_NUM = 4
+MODEL_PATH = f"./model_upconv/split_{SPLIT_NUM}/"
+model_name = "model_CELoss_weighted_36.pth"
 transform_fn = transforms.Compose(
-    [transforms.ToTensor(),
+    [
+        transforms.ToTensor(),
     ]
 )
 
 test_data = SegmentationDataset(DATA_PATH, transform=transform_fn)
 test_dataloader = DataLoader(test_data, batch_size=16)
 
+class_count = torch.zeros(CLASS_NUM)
+for batch, (images, masks) in enumerate(test_dataloader, 1):
+    class_count += masks.unique(return_counts=True)[1]
+
+logger = logging.getLogger("logger")
+logging.getLogger().setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s: %(message)s")
+file_handler = logging.FileHandler(filename=MODEL_PATH + "test.log", mode="w")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+logging.getLogger("logger").info(f"Phase class count: {class_count}")
+logging.getLogger("logger").info(
+    f"Phase class count: {class_count / torch.sum(class_count)} \n"
+)
+
 loss_fn = nn.CrossEntropyLoss()
 confmat = ConfusionMatrix(task="multiclass", num_classes=4)
-dice = Dice(num_classes=4)
-model = UNet(padding=True, up_mode='upsample')
+model = UNet(padding=True, up_mode="upconv")
 model.load_state_dict(torch.load(MODEL_PATH + model_name))
 model.eval()
 
 test_loss = 0
-with torch.no_grad():
-    for batch, (images, masks) in enumerate(test_dataloader, 1):
-        images, masks = images, masks.long()
+with tqdm(total=len(test_data), unit=" batch") as pbar:
+    with torch.no_grad():
+        for batch, (images, masks) in enumerate(test_dataloader, 1):
+            images, masks = images, masks.long()
 
-        # Compute prediction and loss
-        output = model(images)
-        loss = loss_fn(output, masks)
-        test_loss += loss.item()
-        pred_masks = F.softmax(output, dim=1)
-        pred_masks = torch.argmax(pred_masks, dim=1)
-        confmat.update(pred_masks, masks)
-        dice.update(pred_masks, masks)
-        
-test_loss = test_loss/batch
-print(f"test loss: {loss:.3f}")
+            # Compute prediction and loss
+            output = model(images)
+            loss = loss_fn(output, masks)
+            test_loss += loss.item()
+            pred_masks = F.softmax(output, dim=1)
+            pred_masks = torch.argmax(pred_masks, dim=1)
+            confmat.update(pred_masks, masks)
+            pbar.update(images.shape[0])
+    pbar.set_postfix({"Batch": batch, "test loss (in progress)": loss})
 
-#%%
-sample, mask = test_data[2]
+dice = calculate_dice(confmat.confmat)
+accuracy = calculate_accuracy(confmat.confmat)
+test_loss = test_loss / batch
+logging.getLogger("logger").info(f"Test Loss: {test_loss:.3f}")
+logging.getLogger("logger").info(f"Accuracy: {accuracy:.3f}")
+logging.getLogger("logger").info(f"Dice Coefficients: {dice}")
+logging.getLogger("logger").info("\n")
+logging.getLogger("logger").info(f"Confusion matrix:\n{confmat.confmat}")
+handlers = logger.handlers
+for handler in handlers:
+    logger.removeHandler(handler)
+    handler.close()
+
+# %%
+"""
+sample, mask = test_data[5]
 output = model(sample.unsqueeze(0))
 pred_masks = F.softmax(output, dim=1)
 pred_masks = torch.argmax(pred_masks, dim=1)
 show_image(pred_masks.squeeze())
 show_image(mask)
+"""

train.py
@@ -5,19 +5,19 @@ Created on Tue Apr  4 22:07:10 2023
 @author: Yue
 """
 
-import logging 
+import logging
 from pathlib import Path
 
 from tqdm import tqdm
 import torch
-from torch import nn, autocast
+from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchmetrics import ConfusionMatrix
 from sklearn.model_selection import KFold
 
-from utils import calculate_dice
+from utils import calculate_dice, calculate_accuracy
 from unet import UNet
 from dataset import SegmentationDataset, Subset
 
@@ -26,18 +26,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DATA_PATH = "./data/train"
 CLASS_NUM = 4
 N_SPLIT = 4
-MODEL_PATH = f"./model/split_{N_SPLIT}/"
+TRAIN_BS = 16
+VAL_BS = 16
+MODEL_PATH = f"./model_upconv/split_{N_SPLIT}/"
 Path(MODEL_PATH).mkdir(parents=True, exist_ok=True)
 
-logger = logging.getLogger("logger")
-logging.getLogger().setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s")
-file_handler = logging.FileHandler(filename=MODEL_PATH+"train.log", mode="w")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 transform_fn = transforms.Compose(
-    [transforms.ToTensor(),
+    [
+        transforms.ToTensor(),
     ]
 )
 
@@ -50,47 +46,78 @@ train_data = Subset(data, train_indices)
 val_data = Subset(data, val_indices)
 train_dataloader = DataLoader(train_data, batch_size=16, shuffle=True)
 val_dataloader = DataLoader(val_data, batch_size=16)
-#train_features, train_labels = next(iter(train_dataloader))
 class_count = torch.zeros(CLASS_NUM)
 for batch, (images, masks) in enumerate(train_dataloader, 1):
     class_count += masks.unique(return_counts=True)[1]
 
 epochs = 100
-model = UNet(padding=True, up_mode='upsample').to(device)
-optimizer = torch.optim.Adam(model.parameters())
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.5, threshold=0.005)
-loss_weight = torch.Tensor([10, 50, 40, 30]).to(device)
+model = UNet(padding=True, up_mode="upconv").to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer,
+    patience=len(val_data) // VAL_BS * 5,
+    factor=0.5,
+)
+loss_weight = torch.Tensor([1, 1, 1, 1]).to(device)
 loss_fn = nn.CrossEntropyLoss(weight=loss_weight)
+# loss_fn = dice_loss
 best_val_loss = 1000
+scaler = torch.cuda.amp.GradScaler()
+
+logger = logging.getLogger("logger")
+logging.getLogger().setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s: %(message)s")
+file_handler = logging.FileHandler(
+    filename=MODEL_PATH + f"train_CELoss_{loss_weight.cpu().numpy()}.log", mode="w"
+)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 logging.getLogger("logger").info(f"Training on split {N_SPLIT} \n")
 logging.getLogger("logger").info(f"Phase class count: {class_count}")
-logging.getLogger("logger").info(f"Phase class count: {class_count / torch.sum(class_count)} \n")
+logging.getLogger("logger").info(
+    f"Phase class count: {class_count / torch.sum(class_count)} \n"
+)
 logging.getLogger("logger").info(f"CE loss weight: {loss_weight}")
 
-#%%
+best_model = model
+# %%
 for epoch in range(1, epochs + 1):
     model.train()
     epoch_loss = 0
     val_loss = 0
     confmat = ConfusionMatrix(task="multiclass", num_classes=4).to(device)
-    with tqdm(total=len(train_data), unit=" batch", desc=f'Epoch {epoch}/{epochs}') as pbar:
+    with tqdm(
+        total=len(train_data), unit=" batch", desc=f"Epoch {epoch}/{epochs}"
+    ) as pbar:
         for batch, (images, masks) in enumerate(train_dataloader, 1):
             images, masks = images.to(device), masks.to(device, dtype=torch.long)
 
-            # Compute prediction and loss
-            output = model(images)
-            loss = loss_fn(output, masks)
-        
+            # Runs the forward pass with autocasting.
+            with torch.cuda.amp.autocast():
+                output = model(images)
+                loss = loss_fn(output, masks)
+                # loss += dice_loss(output, masks)
+
             # Backpropagation
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # Scales the loss, and calls backward() on the scaled loss to create scaled gradients.
+            scaler.scale(loss).backward()
+            # Since the gradients of optimizer's assigned params are unscaled, clips as usual:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            # optimizer's gradients are already unscaled, so scaler.step does not unscale them,
+            scaler.step(optimizer)
+            # Updates the scale for next iteration.
+            scaler.update()
+
+            # loss.backward()
+            # optimizer.step()
+
             pbar.update(images.shape[0])
             epoch_loss += loss.item()
         epoch_loss /= batch
-        pbar.set_postfix({'Batch': batch, 'Train loss (in progress)': loss})
-        
+        pbar.set_postfix({"Batch": batch, "Train loss (in progress)": loss})
+
         # eval
         model.eval()
         with torch.no_grad():
@@ -103,27 +130,33 @@ for epoch in range(1, epochs + 1):
                 pred_masks = F.softmax(output, dim=1)
                 pred_masks = torch.argmax(pred_masks, dim=1)
                 confmat.update(pred_masks, masks)
-                
+
         dice = calculate_dice(confmat.confmat)
-        pbar.set_postfix({'Train loss (epoch)': epoch_loss, 'Validation loss': val_loss})
+        accuracy = calculate_accuracy(confmat.confmat)
         val_loss /= batch
+        pbar.set_postfix(
+            {"Train loss (epoch)": epoch_loss, "Validation loss": val_loss}
+        )
         logging.getLogger("logger").info(f"Epoch {epoch}")
         logging.getLogger("logger").info(f"Learning rate: {scheduler._last_lr[0]}")
         logging.getLogger("logger").info(f"Train Loss: {epoch_loss:.3f}")
         logging.getLogger("logger").info(f"Validation Loss: {val_loss:.3f}")
-        logging.getLogger("logger").info(f"Dice Coeffients: {dice}")
+        logging.getLogger("logger").info(f"Accuracy: {accuracy:.3f}")
+        logging.getLogger("logger").info(f"Dice Coefficients: {dice}")
         logging.getLogger("logger").info("\n")
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             best_model = model
-            model_name = 'model_{}_{}.pth'.format('CELoss_weighted', epoch)
+            model_name = "model_{}_{}.pth".format("CELoss_weighted", epoch)
         if scheduler._last_lr[0] < 0.00001:
-            logging.getLogger("logger").info("Applying early stopping. Training Terminated.")
+            logging.getLogger("logger").info(
+                "Applying early stopping. Training Terminated."
+            )
             break
-            
+
+logging.getLogger("logger").info(f"Confusion matrix:\n{confmat.confmat}")
 torch.save(best_model.state_dict(), MODEL_PATH + model_name)
 handlers = logger.handlers
 for handler in handlers:
     logger.removeHandler(handler)
     handler.close()
-    

unet.py
@@ -22,7 +22,7 @@ class UNet(nn.Module):
         wf=6,
         padding=False,
         batch_norm=False,
-        up_mode='upconv',
+        up_mode="upconv",
     ):
         """
         Implementation of
@@ -49,7 +49,7 @@ class UNet(nn.Module):
                            'upsample' will use bilinear upsampling.
         """
         super(UNet, self).__init__()
-        assert up_mode in ('upconv', 'upsample')
+        assert up_mode in ("upconv", "upsample")
         self.padding = padding
         self.depth = depth
         prev_channels = in_channels
@@ -108,11 +108,11 @@ class UNetConvBlock(nn.Module):
 class UNetUpBlock(nn.Module):
     def __init__(self, in_size, out_size, up_mode, padding, batch_norm):
         super(UNetUpBlock, self).__init__()
-        if up_mode == 'upconv':
+        if up_mode == "upconv":
             self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
-        elif up_mode == 'upsample':
+        elif up_mode == "upsample":
             self.up = nn.Sequential(
-                nn.Upsample(mode='bilinear', scale_factor=2, align_corners=True),
+                nn.Upsample(mode="bilinear", scale_factor=2, align_corners=True),
                 nn.Conv2d(in_size, out_size, kernel_size=1),
             )
 

utils.py
@@ -6,14 +6,35 @@ Created on Mon May  8 11:52:29 2023
 """
 
 import torch
-from torch import Tensor
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
+
 def show_image(img: torch.Tensor, cmap="gray"):
-    plt.imshow(img, cmap="gray")
+    plt.imshow(img, cmap=cmap)
     plt.show()
-    
-def calculate_dice(confmat: Tensor, num_class=4, smooth=1):
-    dice = {i: (2 * confmat[i, i] + smooth) / (confmat[i, :].sum() + confmat[:, i].sum() + smooth) for i in range(num_class)}
+
+
+def calculate_dice(confmat: torch.Tensor, num_class=4, smooth=1):
+    dice = {
+        i: (2 * confmat[i, i] + smooth)
+        / (confmat[i, :].sum() + confmat[:, i].sum() + smooth)
+        for i in range(num_class)
+    }
     return dice
-        
+
+
+def calculate_accuracy(confmat: torch.Tensor):
+    accuracy = torch.sum(torch.diag(confmat)) / torch.sum(confmat)
+    return accuracy.item()
+
+
+def dice_loss(output: torch.Tensor, masks: torch.Tensor, smooth=1):
+    pred_masks = F.softmax(output, dim=1)
+    one_hot_masks = (F.one_hot(masks, num_classes=4)).permute((0, 3, 1, 2))
+    intersection = pred_masks * one_hot_masks
+    area_total = pred_masks + one_hot_masks
+    intersection_sum = torch.sum(intersection, dim=(-1, -2))
+    area_total_sum = torch.sum(area_total, dim=(-1, -2))
+    dice = (2 * intersection_sum + smooth) / (area_total_sum + smooth)
+    return 1 - torch.mean(dice)