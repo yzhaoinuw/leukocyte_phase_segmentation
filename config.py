41ca1af4ce3ebf866405817c8d0a01f77829cf8b
unknown <yzhaoinuw@gmail.com>
2023-08-24 16:28:49 -0400

    added mask greyscale to class mapping in comments in config.py

--- config.py
+++ config.py
@@ -5,5 +5,10 @@ Created on Wed Aug 23 17:25:39 2023
 @author: Yue
 """
 
-mask_mapping = {209: 0, 188: 3, 157: 2, 76: 1}
+mask_mapping = {
+    209: 0,
+    188: 3,
+    157: 2,
+    76: 1,
+}  # 0: background, 3: probing, 2: phase dark, 1: phase bright
 mask_mapping_reversed = {0: 209, 3: 188, 2: 157, 1: 76}
